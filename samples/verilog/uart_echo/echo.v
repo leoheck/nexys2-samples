8fbbd172866ba9a0cd6e806cf27a217a800ba19f
Leandro Heck <leoheck@gmail.com>
2017-02-11 02:33:34 -0200

    Some progress

--- samples/verilog/uart_echo/echo.v
+++ samples/verilog/uart_echo/echo.v
@@ -11,12 +11,12 @@
 
 //-- Top design
 module echo #(
-         parameter BAUDRATE = `B9600
+	parameter BAUDRATE = `B9600
 )(
-         input wire clk,         //-- System clock
-         input wire rx,          //-- Serial input
-         output wire tx,          //-- Serial output
-         output reg [3:0] leds   //-- Red leds
+	input wire clk,         //-- System clock
+	input wire rx,          //-- Serial input
+	output wire tx,          //-- Serial output
+	output reg [3:0] leds   //-- Red leds
 );
 
 //-- Received character signal
@@ -32,33 +32,35 @@ reg rstn = 0;
 wire ready;
 
 //-- Initialization
-always @(posedge clk)
-  rstn <= 1;
+always @(posedge clk) begin
+	rstn <= 1;
+end
 
 //-- Turn on all the red leds
 //assign leds = 4'hF;
 
 //-- Show the 4 less significant bits in the leds
-always @(posedge clk)
-  leds = data[3:0];
+always @(posedge clk) begin
+	leds = data[3:0];
+end
 
 //-- Receiver unit instantation
-uart_rx #(.BAUDRATE(BAUDRATE))
-  RX0 (.clk(clk),
-       .rstn(rstn),
-       .rx(rx),
-       .rcv(rcv),
-       .data(data)
-      );
+uart_rx #(.BAUDRATE(BAUDRATE)) RX0 (
+	.clk(clk),
+	.rstn(rstn),
+	.rx(rx),
+	.rcv(rcv),
+	.data(data)
+);
 
 //-- Transmitter unit instantation
-uart_tx #(.BAUDRATE(BAUDRATE))
-  TX0 ( .clk(clk),
-         .rstn(rstn),
-         .start(rcv),
-         .data(data),
-         .tx(tx),
-         .ready(ready)
-       );
+uart_tx #(.BAUDRATE(BAUDRATE)) TX0 ( 
+	.clk(clk),
+	.rstn(rstn),
+	.start(rcv),
+	.data(data),
+	.tx(tx),
+	.ready(ready)
+);
 
 endmodule
--- samples/verilog/uart_txchar/txchar.v
+++ samples/verilog/uart_txchar/txchar.v
@@ -2,30 +2,24 @@
 `default_nettype none
 `include "baudgen.vh"
 
-//-- Top entity
 module txchar (
-          input wire clk,   //-- System clock
-          // input wire rstn,  //-- Reset (active low)
-          output wire tx    //-- Serial data output
+	input wire clk,   //-- System clock
+	// input wire rstn,  //-- Reset (active low)
+	output wire tx    //-- Serial data output
 );
 
 reg rstn;
 
-//-- Initialization
-always @(posedge clk)
-  rstn <= 1;
+always @(posedge clk) begin
+	rstn <= 1;
+end
 
-//-- Serial Unit instantation
-uart_tx #(
-    .BAUDRATE(`B9600)  //-- Set the baudrate
-
-  ) TX0 (
-    .clk(clk),
-    .rstn(rstn),
-    .data("A"),    //-- Fixed character to transmit (always the same)
-    .start(1'b1),  //-- Start signal always set to 1
-    .tx(tx)
-);                 //-- Port ready not used
-
+uart_tx #(.BAUDRATE(`B9600)) TX0 (
+	.clk(clk),
+	.rstn(rstn),
+	.data("A"),    //-- Fixed character to transmit (always the same)
+	.start(1'b1),  //-- Start signal always set to 1
+	.tx(tx)
+);
 
 endmodule
